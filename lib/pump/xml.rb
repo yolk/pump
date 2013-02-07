52a3846e5f9295b2a283db4946ae4d0174282010
Sebastian <sebastian@yo.lk>
2013-02-07 18:03:26 +0100

    Pump::Xml: Some cone cleanup

--- lib/pump/xml.rb
+++ lib/pump/xml.rb
@@ -4,50 +4,51 @@ require "pump/xml/tag_array"
 
 module Pump
   class Xml
-    def initialize(root_name, tags, options={})
-      @root_name = root_name
-      @tags = tags
-      @options = options
-      build
+    attr_reader :root_tag_name, :tag_config, :options
+
+    def initialize(root_tag_name, tag_config, options={})
+      @root_tag_name = root_tag_name
+      @tag_config    = tag_config
+      @options       = options
+
+      compile
+    end
+
+    def serialize(object)
+      Array === object ? serialize_array(object) : serialize_single(object)
     end
 
     private
 
-    def build
-      self.instance_eval build_string
+    def compile
+      instance_eval(compile_string)
     end
 
-    def build_string
-      root_node = Tag.new(@root_name, {}, tags, tag_options)
-      array_root_node = TagArray.new(@root_name, {}, tags, tag_options)
+    def compile_string
       <<-EOV
-        def serialize(object)
-          Array === object ? serialize_array(object) : serialize_single(object)
-        end
-
         def serialize_single(object)
-          "#{root_node}"
+          "#{Tag.new(root_tag_name, {}, sub_tags, tag_options)}"
         end
 
         def serialize_array(objects)
-          "#{array_root_node}"
+          "#{TagArray.new(root_tag_name, {}, sub_tags, tag_options)}"
         end
       EOV
     end
 
-    def tags
-      @tags.map do |options|
+    def sub_tags
+      tag_config.map do |options|
         tag_name, method_name = options.keys.first, options.values.first
         Tag.new(tag_name, options[:attributes], Value.new(method_name), options)
       end
     end
 
     def tag_options
-      {:instruct => add_instruct?, :extra_indent => @options[:extra_indent], :array_root => @options[:array_root] }
+      {:instruct => add_instruct?, :extra_indent => options[:extra_indent], :array_root => options[:array_root] }
     end
 
     def add_instruct?
-      @options.has_key?(:instruct) ? @options[:instruct] : true
+      options.has_key?(:instruct) ? options[:instruct] : true
     end
   end
 end
