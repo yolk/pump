3335653168f2037283563b1a7544a3263005fe32
Sebastian <sebastian@yo.lk>
2013-07-23 12:59:06 +0200

    Some dev cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,5 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = 'random'
-
-   config.backtrace_clean_patterns = [/rspec\/(core|expectations)/]
+  config.backtrace_exclusion_patterns = [/rspec\/(core|expectations)/]
 end
