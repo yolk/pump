075bc72c6605189371cb173a9cbbf0db1a94840e
Sebastian <sebastian@yo.lk>
2014-07-10 13:20:32 +0200

    Use Rspec 3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,10 @@
 require File.dirname(__FILE__) + '/../lib/pump.rb'
 
+require 'rspec/its'
+
 XML_INSTRUCT = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = 'random'
